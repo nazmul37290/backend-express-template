cb0b6622d808a5731e8f821775d4479fdd2957d0
Nazmul Haque <124345693+nazmul37290@users.noreply.github.com>
2025-06-15 10:50:47 +0600

    added unhandled rejection and uncaught exception in server.ts

--- src/server.ts
+++ src/server.ts
@@ -14,3 +14,15 @@ async function main() {
 }
 
 main();
+process.on('unhandledRejection', () => {
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+process.on('uncaughtException', () => {
+  process.exit(1);
+});
