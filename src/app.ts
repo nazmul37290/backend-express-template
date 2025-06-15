934e5c73237c5513769eaf0395b0b22e7bc88c73
Nazmul Haque <124345693+nazmul37290@users.noreply.github.com>
2025-06-15 10:53:18 +0600

    added global error handler

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,11 @@ const app: Application = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use('/api', router);
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
-
+app.use(globalErrorHandler);
+app.use(notFoundRoute);
 export default app;
