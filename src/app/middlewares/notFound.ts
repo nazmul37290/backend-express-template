20e24f3fd4b664f24ee6af145264cf3c249675c8
Nazmul Haque <nazmulnjm@gmail.com>
2025-06-15 11:23:21 +0600

    added demo module

--- src/app/middlewares/notFound.ts
+++ src/app/middlewares/notFound.ts
@@ -3,7 +3,7 @@
 import { NextFunction, Request, Response } from 'express';
 
 const notFoundRoute = (req: Request, res: Response, next: NextFunction) => {
-  return res.status(404).json({
+  res.status(404).json({
     success: false,
     message: 'API not found !!',
     error: '',
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import mongoose from 'mongoose';
 import config from './app/config';
 import app from './app';
+import { Server } from 'http';
+let server: Server;
 
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
-    app.listen(config.port, () => {
+    server = app.listen(config.port, () => {
       console.log(`Example app listening on port ${config.port}`);
     });
   } catch (err) {
@@ -19,8 +21,9 @@ process.on('unhandledRejection', () => {
     server.close(() => {
       process.exit(1);
     });
+  } else {
+    process.exit(1);
   }
-  process.exit(1);
 });
 
 process.on('uncaughtException', () => {
